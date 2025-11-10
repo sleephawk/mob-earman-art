be8e9a0f4181c8f96a4d5141f59e116c35c51e0f
Matt <designs.sleephawk@gmail.com>
2025-11-10 10:15:42 +0000

    update bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -18,13 +18,13 @@ export default function Bio({
           <p>
             I once heard someone at a Buddhist commune call their lifestyle
             'Homeless with Style'. Id like to think I was the same, but really,
-            I’m high functioning chaotic as seen on a sofa near you.
+            I’m high-functioning chaotic-good as seen on a sofa near you.
           </p>
           <p>
-            These strange little plays of perspective, miasmic geometry, primary
-            colours and google-y eyes are my therapy whilst I attempt to sort it
-            all out. I make them when I'm too stressed to look at big list on my
-            phone. It works.
+            These strange little oddscapes of warped perspective, wobbly
+            geometry and google-y eyes are my therapy whilst I attempt to sort
+            it all out. I make them when I'm too stressed to look at big list on
+            my phone. It works.
           </p>{" "}
           <p>
             Oh and I’m Mob Earman, don’t ask. I know where the name comes from,
