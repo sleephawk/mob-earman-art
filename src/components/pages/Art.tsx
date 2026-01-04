fe251e57ed36f8aede76a5a2f11b0f92fc5afe39
Matt <designs.sleephawk@gmail.com>
2026-01-04 22:18:35 +0000

    artwork core component added as placeholder on Art page

--- src/components/pages/Art.tsx
+++ src/components/pages/Art.tsx
@@ -1,9 +1,24 @@
+import Artwork from "../core/Artwork";
+
 export default function Art() {
   <section
     aria-label="a catalogue of Mob Earman named original pieces."
     style={{ position: "absolute", width: "100%", height: "100%" }}
   >
     <h1>Art</h1>
-    <div style={{ display: "flex", flexWrap: "wrap", maxWidth: "80%" }}></div>
+    <div style={{ display: "flex", flexWrap: "wrap", maxWidth: "80%" }}>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+      <Artwork src="" alt=""></Artwork>
+    </div>
   </section>;
 }
