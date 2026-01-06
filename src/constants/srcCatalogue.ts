fb449549d6916dfb50ede3c76158a3c57f240b95
Matt <designs.sleephawk@gmail.com>
2026-01-06 11:11:11 +0000

    art images finally looking good. PNGs in correct folder

--- src/constants/srcCatalogue.ts
+++ src/constants/srcCatalogue.ts
@@ -2,43 +2,48 @@ export type Catalogue = Record<string, Record<string, string>>;
 
 export const srcCatalogue: Catalogue = {
   rocky: {
-    noFrame: "/assets/art/photos/noFrame/rockyNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/rockyW.jpg",
+    noFrame: "/assets/art/photos/noFrame/rockyNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/rockyW.png",
     alt: "an abstract cartoonish painting depicting a strangely composed rock formation with eyes on a yellow watercolour background",
   },
   saturn: {
-    noFrame: "/assets/art/photos/noFrame/saturnNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/saturnW.jpg",
+    noFrame: "/assets/art/photos/noFrame/saturnNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/saturnW.png",
     alt: "a black and white pen ink abstract drawing of a cluster of forms headed by a block with two eyes, on a bed of dashed lines and blank white",
   },
   oddSignal: {
-    noFrame: "/assets/art/photos/noFrame/oddNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/oddW.jpg",
+    noFrame: "/assets/art/photos/noFrame/oddNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/oddW.png",
+    alt: "a surreal painting of a tree stump with a lamppost pole that resolved to the head of a flower with an eye, which observes a little monster.",
+  },
+  dragon: {
+    noFrame: "/assets/art/photos/noFrame/dragonNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/dragonW.png",
     alt: "a surreal painting of a tree stump with a lamppost pole that resolved to the head of a flower with an eye, which observes a little monster.",
   },
   autumn: {
-    noFrame: "/assets/art/photos/noFrame/autumnNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/autumnW.jpg",
+    noFrame: "/assets/art/photos/noFrame/autumnNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/autumnW.png",
     alt: "an abstract surreal pen drawing in natural greens and browns depicting a fractured, rocky, leaf-covered fantasy creature with one eye",
   },
   jellyfish: {
-    noFrame: "/assets/art/photos/noFrame/jellyfishNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/jellyfishW.jpg",
+    noFrame: "/assets/art/photos/noFrame/jellyfishNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/jellyfishW.png",
     alt: "a black and white pen abstract drawing of box shapes culminating in a sleeve of cloth, with two eyes at the head of the structure",
   },
   topo: {
     noFrame: "",
-    woodNFrame: "/assets/art/photos/woodNFrame/topoW.jpg",
+    woodNFrame: "",
     alt: "a surreal cartoonish watercolour and pen painting of two odd characters looking at each other longingly, one fractured, one towering.",
   },
   sucker: {
-    noFrame: "/assets/art/photos/noFrame/suckerNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/suckerW.jpg",
+    noFrame: "/assets/art/photos/noFrame/suckerNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/suckerW.png",
     alt: "an abstract brightly coloured array of mathematical looking 3d shapes with eyes and protrusions scattered across the composition.",
   },
   october: {
-    noFrame: "/assets/art/photos/noFrame/octoberNoFrame.jpg",
-    woodNFrame: "/assets/art/photos/woodNFrame/octoberW.jpg",
+    noFrame: "/assets/art/photos/noFrame/octoberNoFrame.png",
+    woodNFrame: "/assets/art/photos/woodNFrame/octoberW.png",
     alt: "an abstract black white and grey pen drawing of mathematical looking 3d shapes, with a mushroom like shape donning one eye in the top left.",
   },
   calendar: {
