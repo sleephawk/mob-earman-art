7f1f889c3711867fc86ff957891706266e83c823
Matt <designs.sleephawk@gmail.com>
2026-01-14 10:03:32 +0000

    main backgrounds for web (not phone) complete & configured

--- src/components/core/VideoBackground.tsx
+++ src/components/core/VideoBackground.tsx
@@ -1,7 +1,7 @@
 export default function VideoBackground({ src }: { src: string }) {
   return (
     <video autoPlay={true} loop={true} className="background-file">
-      <source src={src} />
+      <source src={src} type="video/webm" />
       Your browser does not support webm files
     </video>
   );
